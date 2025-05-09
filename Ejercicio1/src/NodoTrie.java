0691f8a1dbeb0c034e55cd1061f0290dd3cf0352
mariamachado123 <mariae.machadom@correo.ucu.edu.uy>
2025-05-09 15:01:34 -0300

    Actualizacion 3era parte primer ejercico TA2

--- Ejercicio1/src/NodoTrie.java
+++ Ejercicio1/src/NodoTrie.java
@@ -1,2 +1,42 @@
+import java.util.List;
+
 public class NodoTrie {
+    public NodoTrie[] hijos;
+    public boolean esPalabra;
+    public List<Integer> paginas;
+
+    public NodoTrie() {
+        hijos = new NodoTrie[26];
+        esPalabra = false;
+    }
+
+    public void insertarRec(NodoTrie nodo, String palabra, int indice, int pagina) {
+        if (indice == palabra.length()) {
+            nodo.esPalabra = true;
+            if (!nodo.paginas.contains(pagina)) {
+                nodo.paginas.add(pagina);
+            }
+            return;
+        }
+        char caracter = Character.toLowerCase(palabra.charAt(indice));
+        int posicion = caracter - 'a'; /* indice de la letra */
+        if (nodo.hijos[posicion] == null) {
+            nodo.hijos[posicion] = new NodoTrie();
+        }
+        insertarRec(nodo.hijos[posicion], palabra, indice + 1, pagina);
+
+    }
+
+    public void construirIndice(NodoTrie nodo, String palabraActual) {
+        if (nodo.esPalabra) {
+            System.out.println(palabraActual + " " + nodo.paginas);
+        }
+        for (int i = 0; i < 26; i++) { /* recorre todos los hijos */
+            if (nodo.hijos[i] != null) {
+                char siguienteLetra = (char) ('a' + i);
+                construirIndice(nodo.hijos[i], palabraActual + siguienteLetra); /*Crea la palabra*/
+            }
+        }
+    }
+
 }
--- Ejercicio1/src/Trie.java
+++ Ejercicio1/src/Trie.java
@@ -1,2 +1,19 @@
+import java.util.List;
+
 public class Trie {
+    private NodoTrie raiz;
+    public Trie() {
+        raiz = new NodoTrie();
+    }
+    public void insertar(Trie trie, String palabra, int pagina) {
+        if (raiz == null) {
+            raiz = new NodoTrie();
+        }
+        raiz.insertarRec(raiz, palabra, 0, pagina);
+    }
+    public void mostrarIndice() {
+        if (raiz != null) {
+            raiz.construirIndice(raiz, "");
+        }
+    }
 }
